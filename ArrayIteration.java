9d5810d291aa0d9a7ed30c009f48773c4de93764
Suvrat <suvrat72000@gmail.com>
2021-09-29 18:36:16 +0530

    Program to print the elements of Array in the given format

--- ArrayIteration.java
+++ ArrayIteration.java
@@ -1,6 +1,6 @@
 package com.java.tutorials;
 
-import java.lang.reflect.Array;
+
 import java.util.Arrays;
 
 //Program to write the elements of array array[] = {1,2,3,4,5,6}; in following sequence 
@@ -10,7 +10,6 @@ import java.util.Arrays;
 //1234
 //5612
 //3456
-//1234
 public class ArrayIteration {
 
 	public static void main(String[] args) {
@@ -19,17 +18,14 @@ public class ArrayIteration {
 		int arrayCount =0;
 		int tempArray[] = new int[4];
 		for(int j =0; j<tempArray.length*7; j++) {
-			try {
 				tempArray[tempCount] = array[arrayCount];
 				tempCount++;
 				arrayCount++;
-				arrayCount = arrayCount %6;
-			}
-			catch(Exception e) {
-				System.out.println(Arrays.toString(tempArray));
-				tempCount = 0;
-				j=j-1;
-			}
+				if(tempCount==4) {
+					System.out.println(Arrays.toString(tempArray));
+					tempCount = 0;
+				}
+				arrayCount = arrayCount % 6;
 			System.out.println();
 		}
 		
